0b4833f3e8747efcb50e937044fd74e29657ee31
KimJongjae1 <gghs703@naver.com>
2024-11-19 23:58:03 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2296 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
-int main() {
-    int N, c;
-    int b[10001] = { 0 };  // 배열 크기를 10001로 수정
+int main() {//4바이트-
+    int N,c;
+    int b[10002] = { 0, };
     scanf("%d", &N);
 
-    // 입력 값에 대해 등장 횟수를 카운트
     for (int i = 0; i < N; i++) {
         scanf("%d", &c);
-        b[c]++;  // 각 숫자 c의 등장 횟수를 카운트
+        b[c]++;//++가 들어가면 항상 초기화필요
     }
-
-    // 출력: 오름차순으로 숫자 출력
-    for (int i = 1; i <= 10000; i++) {  // 1부터 10000까지 출력
-        if (b[i] > 0) {  // 숫자 i가 등장했다면
-            for (int k = 0; k < b[i]; k++) {  // 해당 숫자만큼 출력
+  
+    for (int i = 1; i <= 10000; i++) {
+         if (b[i] > 0) {
+            for (int k = 0; k < b[i]; k++) {
                 printf("%d\n", i);
             }
-        }
+         }
     }
-    
-    return 0;
+ return 0;
 }
