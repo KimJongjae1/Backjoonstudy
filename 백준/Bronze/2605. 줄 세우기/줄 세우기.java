e37aa7e547415bc4521607dce9798720b801324e
KimJongjae1 <gghs703@naver.com>
2024-12-30 23:48:42 +0900

    [Bronze II] Title: 줄 세우기, Time: 188 ms, Memory: 18292 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.java
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -1,38 +1,27 @@
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
 	
 
 	public static void main(String[] args) throws IOException {
     Scanner scan = new Scanner(System.in);
+    ArrayList<Integer> list = new ArrayList<>();
+    
     
     int N=scan.nextInt();
-    int[] a =new int[N+1];
-    int[] b =new int[N+1];
-    for(int i=1;i<=N;i++) {
-       a[i]=i;
+    
+    for(int i=0;i<N;i++) {
+    	int num =scan.nextInt();
+    	
+    	if(num==0) list.add(i+1);
+    	else {list.add(list.size()-num,i+1);}
     }
     
-    for(int i=1;i<=N;i++) {
-    	int num=scan.nextInt();
-    	if(num!=0) {
-    		int T=num;
-    	   while(num>0) {
-    		   b[i-num+1]=a[i-num];
-    		   num--;
-    	   }
-    	   a[i-T]=a[i];
-    	   while(T>0) {
-    		   a[i-T+1]=b[i-T+1];
-    		   T--;
-    	   }
-    	}
+    for(int i:list) {
+    	System.out.print(i+" ");
     }
-    for(int i=1;i<=N;i++) {
-    System.out.print(a[i]+" ");
-    }
-    		
+    
+    
    }
 }
