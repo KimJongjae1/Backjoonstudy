5ed29a412444945a004a247b453d348fbcc9efe2
KimJongjae1 <gghs703@naver.com>
2025-03-09 22:24:24 +0900

    [Silver III] Title: 자리배정, Time: 152 ms, Memory: 19800 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.java
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -1,48 +1,41 @@
-import java.io.IOException;
-import java.util.Scanner;
-public class Main {
-	
-	
-    public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-  
-       int C=scan.nextInt();
-       int R=scan.nextInt();
-       int K=scan.nextInt();
-       
-       if(K>C*R) {
-    	   System.out.println(0);
-    	   return;
-       }
-       
-       int[][] seat = new int[C+2][R+2];
-       int[] dx = {0,1,0,-1};
-       int[] dy = {1,0,-1,0};
-       int x=1;
-       int y=1;
-       int px=0;
-       int py=0;
-    		   
-       for(int i=1;i<=R*C;i++) {
-    	   seat[x][y]=i;
-    	   
-    	   if(seat[x][y]==K) {
-    		   System.out.print(x+" "+y);
-    		   break;
-    	   }
-    	   x+=dx[px];
-    	   y+=dy[py];
-    	   
-    	   if(x==0||y==0||x==(C+1)||y==(R+1)||seat[x][y]>=1) {
-    		   x-=dx[px];
-    		   y-=dy[py];
-    		   px=(px+1)%4;
-    		   py=(py+1)%4;
-    		   x+=dx[px];
-    		   y+=dy[py];
-    	   }
-       }  
-       
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    }  
-}
+public class Main {
+	static int[] diy= {0,1,0,-1};
+	static int[] dix= {1,0,-1,0};
+	
+     public static void main(String[] args)throws Exception {
+	 BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+     StringTokenizer st =new StringTokenizer(br.readLine());
+     int C=Integer.parseInt(st.nextToken());//가로
+     int R=Integer.parseInt(st.nextToken());//세로
+     int K=Integer.parseInt(br.readLine());
+	    
+	  int[][] map=new int[C+2][R+2];
+      
+	  int idx=0;
+	  int cnt=1;
+	  int y=1;
+	  int x=1;
+     for(int i=0;i<R*C;i++) {
+    	 map[y][x]=cnt;
+    	 if(cnt==K) {
+    		 System.out.println(y+" "+x);
+    		 return;
+    	 }
+    	 cnt++;
+    	 y+=diy[idx]; x+=dix[idx];
+    	 if(y<1||y>C||x<1||x>R||map[y][x]>0) {
+    		 y-=diy[idx]; x-=dix[idx];
+    		 idx++;
+    		 if(idx>=4) idx%=4;
+        	 y+=diy[idx]; x+=dix[idx];
+    	 }
+ 
+     }
+     System.out.println(0);
+    
+     }
+ }
