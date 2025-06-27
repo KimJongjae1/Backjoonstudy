f76a547efea0440f3afc74466742924ce73be82b
KimJongjae1 <gghs703@naver.com>
2025-06-27 19:33:12 +0900

    [Gold I] Title: 놀이 공원, Time: 144 ms, Memory: 15028 KB -BaekjoonHub

--- 백준/Gold/1561. 놀이 공원/놀이 공원.java
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.java
@@ -9,46 +9,45 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static long N;
-    public static int M;
+    public static long M;
+    static long target;
     public static  long[] time;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
         N = Long.parseLong(st.nextToken());
-        M= Integer.parseInt(st.nextToken());
+        M= Long.parseLong(st.nextToken());
         
         if(N<=M) {
         	System.out.println(N);
         	return;
         }
- 
-        time=new long[M+1];
+        target=N-M;
+        time=new long[(int)M+1];
         st=new StringTokenizer(br.readLine());
-        long max=0;
+        long maxtime=0;
         
         for(int i=1;i<=M;i++) {
         	time[i]=Integer.parseInt(st.nextToken());
         }
         
-        max=30*N;
+        maxtime=30*N;
         long min=0;
-        while(min<=max) {
-        	long mid=(min+max)/2;
+        while(min<=maxtime) {
+        	long mid=(min+maxtime)/2;
         	long num=check(mid);//시간 mid는 시간 
         	
-        	if(num<N) min=mid+1;
-        	else max=mid-1;
+        	if(num<target) min=mid+1;
+        	else maxtime=mid-1;
         	
         	
         }
-       
-        
-        long beforechildnum=check(min-1);
+        long ret=0;
+        long beforechildnum=check(min-1)+M;
     
         for(int i=1;i<=M;i++) {
         	if(min%time[i]==0)
         		beforechildnum++;
-        	
         	if(beforechildnum==N) {
         		System.out.println(i);
         		break;
@@ -61,7 +60,7 @@ public class Main {
     }
     public	static long check(long mid) {
     	
-    	long ret=M;
+    	long ret=0;
     	for(int i=1;i<=M;i++) {
     		ret+=mid/time[i];
 
