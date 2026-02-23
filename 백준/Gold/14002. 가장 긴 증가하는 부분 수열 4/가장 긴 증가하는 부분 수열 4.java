b72ebe64d2d06b578e1413e456b6213fc493dd5c
KimJongjae1 <gghs703@naver.com>
2026-02-23 10:26:17 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 128 ms, Memory: 16000 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -4,42 +4,63 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    static List<Integer> list;
+    static int[] arr;
     public static void main(String[] args)throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
 
-        int N=Integer.parseInt(st.nextToken());
-        int[] arr=new int[N];
+         int N=Integer.parseInt(st.nextToken());
+         arr=new int[N];
          st=new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
             arr[i]=Integer.parseInt(st.nextToken());
         }
-        int[] dp=new int[N];
-        Arrays.fill(dp,1);
-        int max=1;
-        for(int i=0;i<N-1;i++){
-            for(int k=i+1;k<N;k++){
-                if(arr[i]<arr[k]){
-                    dp[k]=Math.max(dp[k],dp[i]+1);
-                    max=Math.max(max,dp[k]);
-                }
+
+        int id=0;
+        int[] temp=new int[N];
+        list=new ArrayList<>();
+        for(int i=0;i<N;i++){
+            int idx=lowerbound(arr[i]);
+        
+            if(id==idx){
+                id++;
+                list.add(arr[i]);
+            }else{
+                list.set(idx,arr[i]);
             }
+            temp[i]=idx;
+           
         }
-        StringBuilder sb=new StringBuilder();
-        int id=max;
-        int[] a=new int[max];
+        int[] lis=new int[id];
+        int idx=id-1;
         for(int i=N-1;i>=0;i--){
-            if(dp[i]==id){
-                a[id-1]=arr[i];
-                 id--;
-                if(id<=0)break;
+            if(idx==temp[i]){
+                lis[idx]=arr[i];
+                idx--;
+                if(idx<0)break;
             }
         }
-        for(int i=0;i<max;i++){
-            sb.append(a[i]+" ");
+        
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<list.size();i++){
+            sb.append(lis[i]+" ");
         }
-        System.out.println(max);
+        System.out.println(id);
         System.out.println(sb);
+        
+        
     }
-    
+    public static int lowerbound(int t){
+        int max=list.size();
+        int min=0;
+        while(min<max){
+            int mid=(min+max)/2;
+
+            if(list.get(mid)<t)  min=mid+1;
+            else max=mid;
+            
+        }
+        return min;
+    }
 }
