3505cde481df0d260d69ac58b09d4a534f6f1a08
KimJongjae1 <gghs703@naver.com>
2026-04-16 13:16:28 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -10,7 +10,7 @@ class Main {
     static int[] dp;
      static char[][] arr;
      static List<int[]>[] list;
-     static boolean[] visit;
+     static boolean[][][][] visit;
      static int idx;
      static StringBuilder sb;
      static Set<Integer> set;
@@ -32,128 +32,79 @@ class Main {
             String str=br.readLine();
             for(int k=0;k<M;k++){
                 arr[i][k]=str.charAt(k);
-                if(arr[i][k]=='R')RED=i*M+k;
-                else if(arr[i][k]=='B')BLUE=i*M+k;
+                if(arr[i][k]=='R'){
+                    RED=i*M+k;
+                    arr[i][k]='.';
+                }
+                else if(arr[i][k]=='B'){
+                    BLUE=i*M+k;
+                     arr[i][k]='.';
+                }
             }
         }
-        BACK(0);
+        BFS();
         if(ans==11)System.out.println(-1);
-        else System.out.println(ans+1);
+        else System.out.println(ans);
     }
-    public static void Print(){
-        for(int i=0;i<N;i++){
-            for(int k=0;k<M;k++){
-                System.out.print(arr[i][k]);
-            }System.out.println();
-        }System.out.println();
-    }
-    
-    public static void BACK(int level){
+    public static void BFS(){
+        Queue<int[]> qu=new LinkedList<>();
+        qu.offer(new int[]{RED,BLUE,0});
+        visit=new boolean[N][M][N][M];
+        visit[RED/M][RED%M][BLUE/M][BLUE%M]=true;
+        while(!qu.isEmpty()){
+            int[] cur=qu.poll();
+
+            if(cur[2]>=ans)continue;
+            if(arr[cur[1]/M][cur[1]%M]=='O')continue;
+            if(arr[cur[0]/M][cur[0]%M]=='O'){
+                ans=Math.min(ans,cur[2]);
+                continue;
+            }
+            if(cur[2]>=10)continue;
+            
+            for(int i=0;i<4;i++){
+                int ry=cur[0]/M;
+                int rx=cur[0]%M;
+                int by=cur[1]/M;
+                int bx=cur[1]%M;
+
+                boolean R=false;
+                boolean B=false;
+                while(true){
+
+                   if(!R) {
+                        if(arr[ry+diy[i]][rx+dix[i]]=='O'){
+                            R=true;
+                            ry+=diy[i];rx+=dix[i];
+                        }
+                        else if(ry+diy[i]==by&&rx+dix[i]==bx){
+                            if(B)R=true;
+                        }
+                        else if(arr[ry+diy[i]][rx+dix[i]]!='.') R=true;
+                        
+                        else { ry+=diy[i];rx+=dix[i];}
+                   }
+
+                   if(!B) {
+                        if(arr[by+diy[i]][bx+dix[i]]=='O'){
+                            B=true;
+                            by+=diy[i];bx+=dix[i];
+                        }
+                         else if(by+diy[i]==ry&&bx+dix[i]==rx){
+                            if(R)B=true;
+                        }    
+                        else if(arr[by+diy[i]][bx+dix[i]]!='.') B=true;
+                        else { by+=diy[i];bx+=dix[i];}
+                   } 
+
+                    if(R&&B)break;
+                }
+                if(visit[ry][rx][by][bx])continue;
+                visit[ry][rx][by][bx]=true;
+                
+                qu.offer(new int[]{ry*M+rx,by*M+bx,cur[2]+1});
+            }
+        }
+    }  
  
-        if(level==10) {return;}
-        if(level>=ans)return;
-        
-        char[][] a=new char[N][M];
-        int r=RED;int b=BLUE;
-        COPYARR(a);
-        Boolean B=false;
-        Boolean R=false;
-        if(RED%M<BLUE%M){//right
-           R=RightOrLeft(RED,'R',-1); B=RightOrLeft(BLUE,'B',-1); 
-        }else {
-           B=RightOrLeft(BLUE,'B',-1); R=RightOrLeft(RED,'R',-1);
-        }
-        if(R&&!B) {ans=Math.min(ans,level); return; }
-        if(!R&&!B)BACK(level+1);
-        RETURNARR(a);
-        RED=r;BLUE=b;
-    /////////////////////////////    
-        if(RED%M<BLUE%M){
-          B=RightOrLeft(BLUE,'B',1); R=RightOrLeft(RED,'R',1); 
-        }else {
-            R=RightOrLeft(RED,'R',1); B=RightOrLeft(BLUE,'B',1);
-        }
-        if(R&&!B) {ans=Math.min(ans,level); return;}
-        if(!R&&!B)BACK(level+1);
-        RETURNARR(a);
-        RED=r;BLUE=b;
-/////////////////////////////////
-        if(RED/M<BLUE/M){
-           R=UpOrDown(RED,'R',-1); B=UpOrDown(BLUE,'B',-1);  
-        }else {
-            B=UpOrDown(BLUE,'B',-1);  R=UpOrDown(RED,'R',-1);
-        }
-        if(R&&!B) { ans=Math.min(ans,level);return; }
-        if(!R&&!B)BACK(level+1);
-        RETURNARR(a);
-        RED=r;BLUE=b;
-////////////////////////////
-        if(RED/M<BLUE/M){
-            B=UpOrDown(BLUE,'B',1);R=UpOrDown(RED,'R',1); 
-        }else {
-            R=UpOrDown(RED,'R',1); B=UpOrDown(BLUE,'B',1);  
-        }
-        if(R&&!B) {ans=Math.min(ans,level); return;}
-        if(!R&&!B)BACK(level+1);
-        RETURNARR(a);
-        RED=r;BLUE=b;
-    }
-    public static void RETURNARR(char[][] a){
-       
-        for(int i=0;i<N;i++){
-            for(int k=0;k<M;k++){
-                arr[i][k]=a[i][k];
-            }
-        }
-        
-    }
-     public static void COPYARR(char[][] a){
-        for(int i=0;i<N;i++){
-            for(int k=0;k<M;k++){
-                a[i][k]=arr[i][k];
-            }
-        }
-    }
-    public static boolean RightOrLeft(int n,char type,int dir){
-   
-        int y=n/M;
-        int x=n%M;
-        arr[y][x]='.';
-        while(true){
-            if(arr[y][x+dir]=='O') return true;
-            if(arr[y][x+dir]!='.') break;
-           
-            x+=dir;            
-        }
-        if(type=='B'){
-            arr[y][x]='B';
-            BLUE=y*M+x;
-        }
-        else{ 
-            arr[y][x]='R';
-            RED=y*M+x;
-        }
-        return false;
-    }
-  
-    public static boolean UpOrDown(int n,char type,int dir){
-       
-        int y=n/M;
-        int x=n%M;
-        arr[y][x]='.';
-        while(true){
-            if(arr[y+dir][x]=='O') return true;
-            if(arr[y+dir][x]!='.') break;
-            y+=dir;            
-        }
-        if(type=='B'){
-            arr[y][x]='B';
-            BLUE=y*M+x;
-        }
-        else{ 
-            arr[y][x]='R';
-            RED=y*M+x;
-        }
-        return false;
-    }
 }
